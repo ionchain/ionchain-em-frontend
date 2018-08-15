6b1cac85efecdb4f3d7b5284541bc666031fc271
qiongqiong <1278376741@qq.com>
2018-08-15 17:08:39 +0800

    登录页面

--- pages/login.vue
+++ pages/login.vue
@@ -3,30 +3,30 @@
       <div class="login_left"></div>
       <div class="login_right">
           <div class="login_right_logo">
-              <div><img src="enterprises/img/logo.svg" alt=""></div>
+              <div><img src="../static/icon/logo.svg" alt=""></div>
               <div>IONChain</div>
           </div>
           <div class="login_right_hint">
-              <span><img src="enterprises/img/error.svg" alt=""></span>
+              <span><img src="../static/icon/error.svg" alt=""></span>
               <span>账户名不存在，请重新输入</span>
           </div>
-          <div class="login_right_name"></div>
-          <div class="login_right_pw"></div>
-          <div class="login_right_select">
-              
+          <div class="login_right_name">
+            <input type="text" />
           </div>
-          <div class="login_right_log"></div>
+          <div class="login_right_pw">
+            <input type="text" />
+          </div>
+          <div class="login_right_select">
+              <div><input type="text"></div>
+              <div>注册 ION Chain ID</div>
+          </div>
+          <div class="login_right_log">登录</div>
       </div>
   </div>
 </template>
 <script>
 import '../less/login.less'
 export default {
-  asyncData({ req }) {
-    return {
-      name: req ? 'server' : 'client'
-    }
-  },
   head() {
     return {
       title: `About Page (${this.name}-side)`
