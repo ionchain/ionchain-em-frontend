42151d292314999d67ec1a199649cfead123415b
qiongqiong <1278376741@qq.com>
2018-08-17 18:18:37 +0800

    添加articles页面

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,12 @@
 <template>
   <header class="ly-header">
     <section class="h-main">
-      <a class="logo" href="/">
+      <a class="logo" href="#">
         <img src="/icon/IONC_store_bule.svg" />
       </a>
-      <nav></nav>
-      <div></div>
+      <a class="logo_gw" href="http://www.ionchain.org/index_CN.html" target="_blank">离子链官网</a>
+      <!-- <nav></nav>
+      <div></div> -->
     </section>
   </header>
 </template>
