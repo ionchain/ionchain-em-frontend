170545c51af15778b42f4ab1ceb1a5cdb4b84ab1
cssshine <cssshine06@gmail.com>
2018-08-23 16:39:32 +0800

    控制台打印去掉

--- middleware/auth-router.js
+++ middleware/auth-router.js
@@ -1,7 +1,7 @@
 export default function ({ route, redirect, req }) {
   // console.log('$$$', Object.keys(arguments[0]).join(','), 'process.server>>>', process.server)
   if (process.server && req) {
-    console.log('session@@', req.ctx.session.userinfo)
+    // console.log('session@@', req.ctx.session.userinfo)
     if (!req.ctx.session.userinfo && (req.ctx.path === '/' || req.ctx.path === '/index')) {
       redirect('/login')
     }
