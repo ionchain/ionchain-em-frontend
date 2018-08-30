60c74062cad604266983752a7e0ff1f80466dbf7
qiongqiong <1278376741@qq.com>
2018-08-30 15:19:58 +0800

    index页面已做完

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
     var LIBRARY = '/dist/lib/';
     var PAGES = '/dist/js/pages/';
     var OTHER = '/dist/js/';
+    var PLUGINS = '/plugins/'
     require.config({
         paths: {
             jquery: LIBRARY + 'jquery.min',
@@ -9,12 +10,16 @@
             lodash: LIBRARY + 'lodash.min',
             knockout: LIBRARY + 'knockout',
             'form-serialize': '/lib/form-serialize',
-            validate: LIBRARY + 'validate'
+            validate: LIBRARY + 'validate',
+            tabs: PLUGINS + 'tabs/jquery.tabs',
+            lazyload: PLUGINS + 'tabs/jquery.lazyload'
         },
         map: {
         },
         shim: {
-            api: ['jquery']
+            api: ['jquery'],
+            tabs: ['jquery'],
+            lazyload: ['jquery']
         }
     })
 })(require);
