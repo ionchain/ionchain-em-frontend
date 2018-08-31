9dfcfcd5c338935f093804085501127eca201026
qiongqiong <1278376741@qq.com>
2018-08-31 13:19:51 +0800

    找回

--- src/js/pages/retrieve.js
+++ src/js/pages/retrieve.js
@@ -1,8 +1,22 @@
-require(['jquery','knockout'], function($, KO){
+require(['jquery','knockout', 'serialize', 'validate'], function($, KO, serialize, validate){
     var ViewModel = function(){
+        this.mobile = KO.observable(),
+        this.errorMessage = KO.observable();
+        this.formValid = KO.observable(true);
         this.verify = function(){
-            console.log(123)
+            // console.log('username>>', this.mobile() )
+            var errors = validate(this.mobile(),{
+                mobile:{presence :{message: "^手机号码是必填的"}},
+            });
+            // console.log(errors);
+            // if(errors){
+            //     this.formValid(false);
+            //     this.errorMessage(getMessage(errors))
+            //     return;
+            // }
+            // this.formValid(true);
         }
     };
+
     KO.applyBindings( ViewModel);
 })
