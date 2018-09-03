a03909b85d0a7b5e1871f250a0abe821e10a2ce0
qiongqiong <1278376741@qq.com>
2018-09-03 11:24:18 +0800

    找回密码页面的修改

--- src/customerjs/pages/retrieve.js
+++ src/customerjs/pages/retrieve.js
@@ -1,4 +1,4 @@
-require(['jquery','knockout', 'serialize', 'validate'], function($, KO, serialize, validate){
+require(['jquery','knockout', 'serialize', 'validate', 'common'], function($, KO, serialize, validate,common){
     function getMessage(errors) {
         var msg = [];
         for(var prop in errors) {
@@ -10,18 +10,35 @@ require(['jquery','knockout', 'serialize', 'validate'], function($, KO, serializ
         this.mobile = KO.observable(),
         this.errorMessage = KO.observable();
         this.formValid = KO.observable(true);
-        this.verify = function(){
+        this.verify = function() {
             // console.log('username>>', this.mobile() )
             var errors = validate({mobile: this.mobile()},{
-                mobile:{presence :{message: "^手机号码是必填的"}},
-            });
+                mobile:{
+                  presence :{message: "^手机号码是必填的"},
+                  mobile: true
+                }
+            })
             if(errors){
                 this.formValid(false);
                 this.errorMessage(getMessage(errors))
                 return;
             }
             this.formValid(true);
-        }
+        };
+        this.checkMobile = function() {
+          var errors = validate({mobile: this.mobile()}, {
+            mobile: {
+                presence: {message: "^手机号码是必填的"},
+                mobile: true
+            }
+          })
+          if(errors) {
+              this.formValid(false);
+              this.errorMessage( common.getMessage(errors) )
+          } else {
+              this.formValid(true);
+          }
+        };
     };
 
     KO.applyBindings( ViewModel);
