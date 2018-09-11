aac0b44be5998d0c2fe650de29fdc42a2918248a
cssshine <cssshine06@gmail.com>
2018-09-11 11:58:13 +0800

    首页添加echats

--- src/customerjs/pages/home.js
+++ src/customerjs/pages/home.js
@@ -17,7 +17,7 @@ function ($, API, _, KO, superSlide, knob, echarts) {
             { name: 'Denise' }
         ]);
     }
-    var charts1_opt = {
+    var chartsOpt1 = {
         xAxis: {
             type: 'category',
             boundaryGap: false,
@@ -29,10 +29,50 @@ function ($, API, _, KO, superSlide, knob, echarts) {
         series: [{
             data: [820, 932, 901, 934, 1290, 1330, 1320],
             type: 'line',
-            areaStyle: {}
+            areaStyle: {},
+            smooth: true
         }]
     };
+    var chartsOpt2 = {
+        color: ['#3398DB'],
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis : [
+            {
+                type : 'category',
+                data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'直接访问',
+                type:'bar',
+                barWidth: '60%',
+                data:[10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };
     
+    
+
     $(function() {
         var appviewmodel1 = new AppViewModel();
         KO.applyBindings(appviewmodel1, $(".page-home")[0]);
@@ -50,5 +90,11 @@ function ($, API, _, KO, superSlide, knob, echarts) {
             mainCell: ".slide-wrap",
             effect: "leftLoop"
         });
+
+        var chart1 = echarts.init(document.getElementById('chart1'));
+        chart1.setOption(chartsOpt1);
+        var chart2 = echarts.init(document.getElementById('chart2'));
+        chart2.setOption(chartsOpt2);
+
     });
 })
