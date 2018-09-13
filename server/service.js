da40809db8df761d1459f5e3168d96de27e7f437
cssshine <cssshine06@gmail.com>
2018-09-13 10:20:11 +0800

    修改service

--- server/service.js
+++ server/service.js
@@ -2,13 +2,11 @@ const API = require('./api.js')
 
 module.exports = {
     async getDeviceList({userId} = {}){
-        var data = null
+        var data = []
         try{
             await API.getDeviceList({userId}).then((res)=>{
                 if( res.data.success == 0 ){
                     data = res.data.data
-                } else {
-                    data = null
                 }
             })
         } catch (err) {
@@ -18,13 +16,11 @@ module.exports = {
         return data
     },
     async getDeviceDesc({deviceId} = {}){
-        var data = null
+        var data = {}
         try{
             await API.getDeviceDesc({deviceId}).then((res)=>{
                 if( res.data.success == 0 ){
                     data = res.data.data
-                } else {
-                    data = null
                 }
             })
         } catch (err) {
