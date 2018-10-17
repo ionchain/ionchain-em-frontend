6e17a746a0a61cc1a23ce058017ed1acdc22ecf1
qiongqiong <1278376741@qq.com>
2018-10-17 09:35:51 +0800

    添加收藏功能

--- src/customerjs/pages/download_can.js
+++ src/customerjs/pages/download_can.js
@@ -1,19 +1,54 @@
 require(['jquery','knockout','validate', 'api', 'common'], function($, KO, validate, API, common){
     function AppViewModel() {
+        // this.collectClick = function(m, event){
+        //    var param = $(event.target).data('param');
+        //     API.collectCode(param)._then(function(data){
+        //         console.log(data)
+        //         if(data.success == 0){
+        //             $.toast({text: '收藏成功', icon: 'success'});
+        //             location.reload();
+        //         }else{
+        //             $.toast({text: data.message, icon: 'error'});
+        //         }
+        //     }) 
+        // }
+        // this.cancelClik = function(m, event){
+        //     var param = $(event.target).data('param');
+        //     API.cancelCode(param)._then(function(data){
+        //         if(data.success == 0){
+        //             $.toast({text: '取消收藏', icon: 'success'});
+        //             location.reload();
+        //         }else{
+        //             $.toast({text: data.message, icon: 'error'});
+        //         }
+        //     })
+        // }
         this.collectClick = function(m, event){
-           var param = $(event.target).data('param');
-            API.collectCode(param)._then(function(data){
-                console.log(data)
-                if(data.success == 0){
-                    $.toast({text: '收藏成功', icon: 'success'});
-                    $(".t_third").addClass("active");
-                }else{
-                    $.toast({text: data.message, icon: 'error'});
-                }
-            }) 
-        }
-        this.cancelClik = function(m, event){
-            // API.cancelCode()
+            var whether = $(event.target).data('whether');
+            if(whether != true){
+                var collect = $(event.target).data('collect');
+                API.collectCode(collect)._then(function(data){
+                    // console.log(data)
+                    if(data.success == 0){
+                        $(event.target).data('whether', true);
+                        $(event.target).data('cancel',{id: data.data.id});
+                        // canel_id.id = data.data.id;
+                        $(".t_third>button").addClass("active");
+                    }else{
+                        $.toast({text: data.message, icon: 'error'});
+                    }
+                }) 
+            }else{
+                var cancel = $(event.target).data('cancel');
+                API.cancelCode(cancel)._then(function(data){
+                    if(data.success == 0){
+                        $(event.target).data('whether', false);
+                        $(".t_third>button").removeClass("active");
+                    }else{
+                        $.toast({text: data.message, icon: 'error'});
+                    }
+                })
+            }
         }
     }
 
